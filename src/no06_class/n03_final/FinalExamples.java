916337a6dba6a63d9a3b316fce5efab194e86bab
JarvisYu <JarvisYu_Cn@outlook.com>
2021-03-01 09:20:18 +0000

    added 0603.'final in classes'

---
+++ src/no06_class/n03_final/FinalExamples.java
@@ -0,0 +1,21 @@
+package no06_class.n03_final;
+
+public class FinalExamples {
+}
+
+/** The "final" means class cannot be subclassed. */
+final class A {
+  // TODO
+}
+
+class B {
+
+  public void f1() {
+    // TODO
+  }
+
+  /** The "final" means method cannot be overridden. */
+  public final void f2() {
+    // TODO
+  }
+}
